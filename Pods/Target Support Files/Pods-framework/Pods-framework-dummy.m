948e01bdd7b169fb639d2bcfc778ad6fc4dc742a
Sascha, Balkau | PAYSD <ts-balkau.sascha@rakuten.com>
2018-05-09 17:01:38 +0900

    Erase again

--- Pods/Target Support Files/Pods-framework/Pods-framework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_framework : NSObject
-@end
-@implementation PodsDummy_Pods_framework
-@end
--- Pods/Target Support Files/autumn/autumn-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_autumn : NSObject
-@end
-@implementation PodsDummy_autumn
-@end
