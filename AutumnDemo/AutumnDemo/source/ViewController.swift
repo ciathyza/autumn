0776741c1aa5530af35782b5746e1d5bfb2bd68a
Sascha, Balkau | PAYSD <ts-balkau.sascha@rakuten.com>
2018-05-09 17:13:30 +0900

    New Xcode Project Setup

---
+++ AutumnDemo/AutumnDemo/source/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutumnDemo
+//
+//  Created by Sascha, Balkau | FINAD on 2018/05/09.
+//  Copyright © 2018 Ciathyza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ AutumnFramework/AutumnFramework/source/AutumnFramework.h
@@ -0,0 +1,17 @@
+//
+//  AutumnFramework.h
+//  AutumnFramework
+//
+//  Created by Sascha, Balkau
+//
+
+#import <UIKit/UIKit.h>
+
+
+//! Project version number for AutumnFramework.
+FOUNDATION_EXPORT double AutumnFrameworkVersionNumber;
+
+//! Project version string for AutumnFramework.
+FOUNDATION_EXPORT const unsigned char AutumnFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutumnFramework/PublicHeader.h>
